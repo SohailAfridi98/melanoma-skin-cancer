613a177a23a3da3a688026dd3b2a8206701533c2
MuhammadSadeeq <afridi.sadeeq.m@gmail.com>
2020-06-07 18:21:42 +0500

    basic features implemented

---
+++ lib/pages/main.dart
@@ -0,0 +1,78 @@
+import 'package:deyegnose/pages/statistics.dart';
+import 'package:flutter/material.dart';
+
+import 'history.dart';
+import 'home.dart';
+import 'package:dcdg/dcdg.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  String _value;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'dEYEgnose',
+      debugShowCheckedModeBanner: false,
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  int _currentIndex = 0;
+  List _listPages = List();
+  Widget _currentPage;
+  @override
+  void initState() {
+    super.initState();
+    _listPages..add(Home())..add(StatisticsScreen())..add(History());
+    _currentPage = Home();
+  }
+
+  void _changePage(int selectedIndex) {
+    setState(() {
+      _currentIndex = selectedIndex;
+      _currentPage = _listPages[selectedIndex];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('dEYEgnose'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: _currentPage,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.insert_chart),
+            title: Text('Statistics'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            title: Text('History'),
+          ),
+        ],
+        onTap: (selectedIndex) => _changePage(selectedIndex),
+      ),
+    );
+  }
+}
